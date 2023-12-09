d53bf3f3bb98ec920874ff798cbe630ddcafe63c
Konstantin Shalimov <90028811+VladKsenobayt@users.noreply.github.com>
2023-12-09 13:10:36 +0300

    Update Warehouse.py

--- Warehouse.py
+++ Warehouse.py
@@ -2,11 +2,13 @@ import pygame
 
 
 class Warehouse(pygame.sprite.Sprite):
-    def __init__(self, size, color, x, y):
+    def __init__(self, size=None, color=0, x=0, y=0):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill(color)
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.number = 0
+        if size is not None:#в целях тестирования
+            self.image = pygame.Surface((size, size))
+            self.image.fill(color)
+            self.rect = self.image.get_rect(topleft=(x, y))
 
     shape = [
         'xxxxxxxxxxxxxxxx',
